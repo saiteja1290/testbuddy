50ccefc15b5ecaf91851908ebdc4b08d12e18303
saiteja.palegar@gmail.com <saiteja.palegar@gmail.com>
2024-06-18 22:59:19 +0530

    created pages

---
+++ frontend/src/pages/Roompage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// It gives random room code which is to be used by stundets to pariticipate in the exam, 
+// It gives teacher to set questions
+// View Results Table
+const Roompage = () => {
+    return (
+        <div>Roompage</div>
+    )
+}
+
+export default Roompage
---
+++ frontend/src/pages/TeacherDashboard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+//Teacher can set questions, set room, view results - create 3 seperate buttons
+const TeacherDashboard = () => {
+    return (
+        <div>TeacherDashboard</div>
+    )
+}
+
+export default TeacherDashboard
