1f4f35e78b229350e36c479146f3513e1e8cf75e
saiteja.palegar@gmail.com <saiteja.palegar@gmail.com>
2024-06-19 00:25:36 +0530

    basic backend

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,47 @@
-console.log("Index");
-console.log("TEst Buddy");
+import express, { response } from "express";
+// import { PORT, mongourl } from "./config.js";
+// import dotenv from "dotenv";
+import mongoose from "mongoose";
+// import booksRoute from "./routes/booksRoute.js";
+// import { Book } from "./models/bookmodel.js";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config()
+//express
+const app = express();
+// const cors = require('cors');
+app.use(express.json());
+//CORS Policy 
+// app.use(cors(
+//     // {
+//     //     origin: ["https://deploy-mern-1whq.vercel.app"],
+//     //     methods: ['POST', 'GET'],
+//     //     credentials: true
+//     // }
+// ))
+app.use(cors())
+app.get('/', (req, res) => {
+    console.log(req)
+    return response.status(200).send("Welcome to MERN stack Project")
+});
+
+// app.use('/books', booksRoute)
+
+
+
+//Route to save a new book
+
+//Mongoose
+// PORT = 8080
+mongoose
+    .connect(process.env.mongo_url)
+    .then(() => {
+        console.log('App connected to database');
+        app.listen(8080, () => {
+            console.log("App is listening to port: 8080");
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
---
+++ backend/models/examSchema.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+// Define the schema for a question
+const questionSchema = new mongoose.Schema({
+    questionText: {
+        type: String,
+        required: true
+    },
+    testCases: {
+        type: String,
+        required: true
+    },
+    answers: {
+        type: String,
+        required: true
+    }
+});
+
+// Define the schema for the exam settings
+const examSchema = new mongoose.Schema({
+    roomId: {
+        type: String,
+        required: true
+    },
+    numQuestions: {
+        type: Number,
+        required: true
+    },
+    studentQuestions: {
+        type: Number,
+        required: true
+    },
+    questions: [questionSchema]
+});
+
+// Create the model for the exam
+export const Exam = mongoose.model('Exam', examSchema);
+
+module.exports = Exam;
