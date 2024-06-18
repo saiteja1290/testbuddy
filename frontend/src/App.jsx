13f580a29278f434fce030620cb549d5b97087a9
saiteja.palegar@gmail.com <saiteja.palegar@gmail.com>
2024-06-18 22:24:05 +0530

    created routes and made pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
-
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import AdminSignup from './pages/AdminSignup';
+import StudentSignup from './pages/StudentSignup';
+import StudentLogin from './pages/StudentLogin';
+import AdminLogin from './pages/AdminLogin';
 function App() {
 
   return (
-    <>
-
-    </>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/studentsignup' element={<StudentSignup />} />
+      <Route path='/adminsignup' element={<AdminSignup />} />
+      <Route path='/studentlogin' element={<StudentLogin />} />
+      <Route path='/adminlogin' element={<AdminLogin />} />
+    </Routes>
   )
 }
 
