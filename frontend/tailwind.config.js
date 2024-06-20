981d888b57a3beebdae5ffe170c10171407533cd
yogireddy21 <yogeswarreddyrachamallu@gmail.com>
2024-06-21 00:33:56 +0530

    home page done

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "indigo-800": "#1E3A8A",
+        "indigo-500": "#6366F1",
+        "gray-100": "#F3F4F6",
+        "gray-800": "#1F2937",
+        "indigo-600": "#4F46E5",
+      },
+    },
   },
   plugins: [],
-}
-
+};
