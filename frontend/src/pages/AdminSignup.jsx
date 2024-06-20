7b886dd1d981ff3f18855b4c3d44e3235a8918ae
saiteja.palegar@gmail.com <saiteja.palegar@gmail.com>
2024-06-20 22:00:46 +0530

    question setting - backend and frontend link done

--- frontend/src/pages/AdminSignup.jsx
+++ frontend/src/pages/AdminSignup.jsx
@@ -57,9 +57,6 @@ const AdminSignup = () => {
 
                 <p className="mt-10 text-center text-sm text-gray-500">
                     Not a member?{' '}
-                    <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-                        Start a 14 day free trial
-                    </a>
                 </p>
             </div>
         </div>
