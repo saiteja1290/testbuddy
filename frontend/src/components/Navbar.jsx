80aedde417d108972dd498eec777e409e0faf768
saiteja.palegar@gmail.com <saiteja.palegar@gmail.com>
2024-06-21 13:06:23 +0530

    updated navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -41,8 +41,8 @@ const Navbar = () => {
     return (
         <nav className="bg-indigo-800 dark:bg-gray-800">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
-                    <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
+                <a href="" className="flex items-center space-x-3 rtl:space-x-reverse">
+                    <img src="" className="h-8" alt="Flowbite Logo" />
                     <span className="self-center text-2xl font-semibold whitespace-nowrap text-gray-100">TestBuddy</span>
                 </a>
                 <button id="navbar-toggle" data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-100 rounded-lg md:hidden hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-600 dark:text-gray-100 dark:hover:bg-indigo-600 dark:focus:ring-indigo-600" aria-controls="navbar-default" aria-expanded="false">
